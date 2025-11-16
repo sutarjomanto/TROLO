ca30a47a45aa8f6375f35aa4387db556583daabf
Naufal adiva <adivalisna11@gmail.com>
2025-11-16 20:50:59 +0700

    update

--- komponen/Navbar.js
+++ komponen/Navbar.js
@@ -1,13 +1,7 @@
 export default function Navbar() {
   return (
     <nav style={{padding: 20, background: 'rgba(255, 255, 255, 1)', color: '#000000ff'}}>
-      <div style={{
-  fontWeight: '700',
-  fontFamily: 'Poppins, sans-serif'
-}}>
-  <center>TROLO</center>
-</div>
-
+       <div style={{fontWeight:'700'}}><center></center>  </div>
     </nav>
   )
 }
