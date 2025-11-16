2e1b028feb14d0cc6b0dd3a72fc356c0bc4256e5
Naufal adiva <adivalisna11@gmail.com>
2025-11-16 21:13:49 +0700

    update

--- komponen/Navbar.js
+++ komponen/Navbar.js
@@ -1,23 +1,12 @@
 export default function Navbar() {
+  const backgroundColor = "#ffffff"
+  const textColor = "#000000"
+
   return (
-    <nav
-      style={{
-        padding: 20,
-        background: "#f0f0f0",
-        color: "#000",
-        borderBottom: "1px solid #ddd"
-      }}
-    >
-      <div
-        style={{
-          fontWeight: "700",
-          fontFamily: "Poppins, sans-serif",
-          textAlign: "center",
-          fontSize: 22
-        }}
-      >
+    <nav style={{ padding: 20, background: backgroundColor, color: textColor }}>
+      <center style={{ fontWeight: 700, fontFamily: "Poppins, sans-serif" }}>
         TROLO
-      </div>
+      </center>
     </nav>
-  );
+  )
 }
